37efdcac81ee2d0d7fb27717d1ab8561d5aec591
Knight <janae.jpeg@gmail.com>
2019-11-04 14:35:43 -0600

    gen update

--- java/javascript.js
+++ java/javascript.js
@@ -7,11 +7,24 @@ function generalFunction() {
     var yearMenu = document.getElementById("yearInput");
     var selYear = yearMenu.options[yearMenu.selectedIndex].text;
 
-    console.log("User picked the date: " +selDate, "" +selYear, ".");
+    console.log("User picked the date: " +selDate, "" +selMonth, "" +selYear, ".");
 
     if (selYear<=2000) {
-        console.log("Sure");
-        window.location.replace("xxtraoily.html");
-    }; 
+        console.log("User is over 18 ((auto)).");
+        //window.location.replace("xxtraoily.html");
+    } else if (selMonth === "December") {
+        console.log("Calculating...");
+            if (selDate>=5 && selYear===2001) {
+                console.log("User is over 18 ((manual)).");
+                //window.location.replace("xxtraoily.html");
+            } else {
+                console.log("User is under 18 ((manual)).");
+            }
+    } else if (selYear>2001) {
+        console.log("User is under 18 ((manual)).");
+    } else {
+        console.log("User is over 18 ((manual)).");
+    }
+;
     return;
 };
