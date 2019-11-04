4026ea75fd8d9b001d0022943f0d46441e242b72
Knight <janae.jpeg@gmail.com>
2019-11-04 11:24:52 -0600

    gen update

--- java/javascript.js
+++ java/javascript.js
@@ -1,6 +1,13 @@
 function generalFunction() {
+
     var yearMenu = document.getElementById("yearInput");
     var selYear = yearMenu.options[yearMenu.selectedIndex].text;
-    console.log(selYear);
+    console.log("User picked the year: " +selYear, ".");
+
+    if (selYear<=2000) {
+        console.log("Sure");
+        window.location.replace("janaeknight.github.io");
+    }
+    ; 
     return;
 };
