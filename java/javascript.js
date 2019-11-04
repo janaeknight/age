a122650c4a89dc5ebf86862589d97228f73127f5
Knight <janae.jpeg@gmail.com>
2019-11-04 13:14:50 -0600

    gen update

--- java/javascript.js
+++ java/javascript.js
@@ -1,13 +1,17 @@
 function generalFunction() {
 
+    var dayMenu = document.getElementById("dayInput");
+    var selDate = dayMenu.options[dayMenu.selectedIndex].text;
+    var monthMenu = document.getElementById("monthInput");
+    var selMonth = monthMenu.options[monthMenu.selectedIndex].text;
     var yearMenu = document.getElementById("yearInput");
     var selYear = yearMenu.options[yearMenu.selectedIndex].text;
-    console.log("User picked the year: " +selYear, ".");
+
+    console.log("User picked the date: " +selDate, "" +selYear, ".");
 
     if (selYear<=2000) {
         console.log("Sure");
-        window.location.replace("janaeknight.github.io");
-    }
-    ; 
+        window.location.replace("xxtraoily.html");
+    }; 
     return;
 };
